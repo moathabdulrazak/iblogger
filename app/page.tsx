26a8f8fecc2443d0532c4acffd54f7b97e9bfd59
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-27 04:18:30 -0600

    starting fresh, other had too many errors

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+    <h3 className='text-4xl font-bold underline' >Welcome to the blog</h3>
+    </div>
+  )
+}
+
+export default HomePage
