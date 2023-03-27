1fbc6ac60f0a2a83e2ddf60eead816c192068a6b
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-27 04:38:00 -0600

    s

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,12 +3,15 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 export default defineConfig({
-  name: 'default',
-  title: 'sanity blog',
+  basePath:"/studio",
+  name: 'MOMO_Content_Studio',
+  title: 'MOMO Content Studio',
 
-  projectId: 'b7mjl7r4',
-  dataset: 'production',
+  projectId,
+  dataset,
 
   plugins: [deskTool(), visionTool()],
 
