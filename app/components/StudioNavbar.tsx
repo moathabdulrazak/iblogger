ded3799161ac0ce69c383f9ab59d10ac48e92126
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-27 06:26:00 -0600

    small

--- app/components/StudioNavbar.tsx
+++ app/components/StudioNavbar.tsx
@@ -8,7 +8,7 @@ const StudioNavbar = (props: any) => {
       <div className='flex items-center' >
         <Link className='text-[#f4b400] flex items-center text-color'  href="/">
           <ArrowUturnLeftIcon className=' arrowSize text-[#f4b400] h-6 w-6 mr-2 ' />
-        Go to website
+        Go to frontend
         </Link>
       </div>
       <>{props.renderDefault(props)}</>
