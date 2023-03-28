40f40cb4104243ffb48d8100e5c1eb1b399f7366
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-28 03:30:18 -0600

    working on schemas. and implmenting them

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,12 +1,44 @@
 import React from 'react'
 import { previewData } from 'next/headers'
+import { groq } from 'next-sanity'
+import { client } from '@/lib/sanity.client'
+import { PreviewSuspense } from 'next-sanity/preview'
+import PreviewBlogList from '@/components/PreviewBlogList'
+import BlogList from '@/components/BlogList'
+const query = groq`
+  *[_type == 'post'] {
+    ...,
+    author->,
+    categories[]->
+  } | order(_createdAt desc)
+  
+`
+function PreviewSuspenseFallback() {
+  return (
+    <div role="status">
+      <p className="animate-pulse text-center text-lg text-zinc-800">
+        Loading Preview Data...
+      </p>
+    </div>
+  )
+}
 
-export default function HomePage() {
+export default async function HomePage() {
   if (previewData()) {
-    return <div>Preview mode</div>
+    return (
+      <PreviewSuspense
+        fallback={<PreviewSuspenseFallback></PreviewSuspenseFallback>}
+        
+        >
+          <PreviewBlogList query={query} />
+      </PreviewSuspense>
+    )
   }
 
-  return <div>
-    <h1>Not in preview mode</h1>
+
+  const posts = await client.fetch(query);
+  return (<div>
+    <BlogList posts={posts} />
   </div>
+  )
 }
---
+++ components/BlogList.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+  posts: Post;
+};
+
+const BlogList = ({posts}: Props) => {
+  return (
+    <div>BlogList</div>
+  )
+}
+
+export default BlogList
