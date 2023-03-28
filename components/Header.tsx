7f7afb62dc7cec158570fc34430ab073ea8988b9
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-28 01:33:54 -0600

    somethings working

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,27 @@
 import Image from 'next/image'
 import Link from 'next/link'
-
+// import '../styles/user-styles'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='flex items-center justify-between space-x-2 font-bold pc-10 py-5'>
+      <div className='flex items-center space-x-2' >
+        <Link href="/">
+        <Image 
+      className="object-cover"
+      width={50}
+      height={50}
+      src="https://static.vecteezy.com/system/resources/previews/000/615/271/large_2x/letter-m-vector-icons-such-logos-template.jpg"
+      alt="brand logo"
+         />
+        </Link>
+        <h1>Iblogger</h1>
+      </div>
+
+
+      <div></div>
+    </header>
   )
 }
 
