6a0db65d9330c4e94fca0cdc9555cc74570462c9
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-28 01:20:22 -0600

    component building

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {}
       <body>{children}</body>
     </html>
   )
---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
