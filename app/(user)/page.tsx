c6a1a47d17b652a61357acb8538774153962c650
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-28 02:22:37 -0600

    banner completed

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-    <h3 className='text-4xl ' >Welcome to the blog</h3>
     </div>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,33 +1,35 @@
 import Image from 'next/image'
 import Link from 'next/link'
-// import '../styles/user-styles'
-import React from 'react'
+import { GlobeAltIcon, UserGroupIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
   return (
     <header className='flex items-center justify-between space-x-2 font-bold pc-10 py-5'>
-      <div className='flex items-center space-x-2' >
-        <Link href="/">
-        <Image 
-      className="object-cover"
-      width={50}
-      height={50}
-      src="https://static.vecteezy.com/system/resources/previews/000/615/271/large_2x/letter-m-vector-icons-such-logos-template.jpg"
-      alt="brand logo"
-         />
+      <div className='flex items-center space-x-2'>
+        <Link href='/'>
+          <Image 
+            className='object-cover'
+            width={75}
+            height={75}
+            src='https://static.vecteezy.com/system/resources/previews/000/615/271/large_2x/letter-m-vector-icons-such-logos-template.jpg'
+            alt='brand logo'
+          />
         </Link>
         <h1>Iblogger</h1>
+        <div className='flex items-center space-x-2'>
+        </div>
       </div>
 
-
       <div className='flex items-center space-x-2 ml-2 pr-5'>
-      <Link href="/studio">
-        <h1 className='px-5 py-3 text-sm md:text-base bg-gray-900 text-[#F7AB0A] flex items-center rounded-full text-center'>
-          Post
-        </h1>
-      </Link>
-      <p className="text-gray-500 text-sm">(if you're authorized by admin)</p>
-    </div>
+        
+        <Link href='/studio'>
+          <h1 className='px-5 py-3 text-sm md:text-base bg-gray-900 text-[#F7AB0A] flex items-center rounded-full text-center'>
+            Post
+          </h1>
+        </Link>
+        <p className='text-gray-500 text-sm'>(if you're authorized by admin)</p>
+
+      </div>
     </header>
   )
 }
