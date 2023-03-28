5bdf14eb8cf05661c7dc4a5c5a7bc95ee4311d6b
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-27 19:31:06 -0600

    group routes

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,8 +3,8 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 import { myTheme } from './theme';
-import StudioNavbar from './app/components/StudioNavbar';
-import Logo from './app/components/Logo';
+import StudioNavbar from './components/StudioNavbar';
+import Logo from './components/Logo';
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 export default defineConfig({
